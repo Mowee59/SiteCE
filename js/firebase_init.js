d9453bf41679a99b1e58798d969a61c94bb33338
Unknown <mahfoudi.apb@live.fr>
2018-03-14 11:20:22 +0100

    Nettoyage
    Nettoyage fichiers inutiles + début firebase

---
+++ js/firebase_init.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyB8qIh5qXbfgO1Bh34_9YXwg924TR0YfK8",
+    authDomain: "projet-cf4f8.firebaseapp.com",
+    databaseURL: "https://projet-cf4f8.firebaseio.com",
+    projectId: "projet-cf4f8",
+    storageBucket: "",
+    messagingSenderId: "830951586660"
+};
+firebase.initializeApp(config);
