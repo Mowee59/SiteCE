bae136b8cb9fc079034e1d3f8f05e8e6b74227cb
Unknown <mahfoudi.apb@live.fr>
2018-03-14 10:46:31 +0100

    Style  barre recherche js

---
+++ js/main.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var barre_recherche = (function () {
+
+    var search = document.getElementById("search");
+    var adv = document.getElementById("adv");
+    var input = document.getElementById("input-nav");
+    input.addEventListener('focus', function (event) {
+        
+        search.classList.add("btn-search-focus");
+        adv.classList.add("btn-barre-focus");
+
+    });
+    
+    input.addEventListener('blur', function (event) {
+        search.classList.remove("btn-search-focus");
+
+
+    });
+
+})();
